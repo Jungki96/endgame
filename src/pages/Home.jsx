af35d36db60df4cfe262c1e141bfd901dd8561ed
Jungki <andy960609@gmail.com>
2023-01-07 12:27:15 +0900

    로직 세팅 및 구조 이해 단계

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../shared/Layout";
+
+const List = () => {
+  return (
+    <Layout>
+      <div className="listGroup">
+        <div className="listWorking">🥕🥕🥕🥕🥕</div>
+        <div className="oneList"></div>
+        <div>🧡💛💚💙💜</div>
+        <div className="oneList"></div>
+      </div>
+    </Layout>
+  );
+};
+
+export default List;
